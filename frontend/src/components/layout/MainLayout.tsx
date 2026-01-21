10dea83736f157119ab8558ecceb694cab81e64c
EyeKonick <delsocoravillyjoe02@gmail.com>
2026-01-21 13:37:21 +0800

    Layout Updating

--- frontend/src/components/layout/MainLayout.tsx
+++ frontend/src/components/layout/MainLayout.tsx
@@ -4,12 +4,14 @@ import Sidebar from './Sidebar';
 
 export default function MainLayout() {
   return (
-    <div className="flex flex-col h-screen w-screen overflow-hidden bg-[#0a0e27]">
+    <div className="flex flex-col h-screen w-screen overflow-hidden">
       <Header />
+      
       <div className="flex flex-1 min-h-0">
         <Sidebar />
-        <main className="flex-1 overflow-y-auto">
-          <div className="max-w-[1400px] mx-auto p-8">
+        
+        <main className="flex-1 overflow-y-auto bg-[#0f172a]">
+          <div className="max-w-7xl mx-auto p-6">
             <Outlet />
           </div>
         </main>
